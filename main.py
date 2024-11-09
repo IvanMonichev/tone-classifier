0daa67a3acd1834578f51362b88ebf47ebe1db86
IvanMonichev <monichevid@baltech.ru>
2024-11-09 13:44:42 +0300

    feat: prepare datasets for analysis

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import pandas
+from sklearn.model_selection import train_test_split
 
 
+def create_tweet_dataframe():
+    positive = pandas.read_csv('./content/positive.csv', sep=';', usecols=[3], names=['text'])
+    negative = pandas.read_csv('./content/negative.csv', sep=';', usecols=[3], names=['text'])
 
+    positive['label'] = ['positive'] * len(positive)
+    negative['label'] = ['negative'] * len(negative)
+
+    return pandas.concat([positive, negative])
+
+
+def split_data(texts, labels):
+    x_train, x_test, y_train, y_test = train_test_split(texts, labels)
 
 if __name__ == '__main__':
-    print('Hello')
+    dataframe = create_tweet_dataframe()
+    split_data(dataframe['text'], dataframe['label'])
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # Tone Classifier
+
+## Скачивание файлов
+
+Этот проект требует скачивания двух файлов в папку `content`. Чтобы это сделать, выполните следующую команду:
+
+### Linux / MacOS / WSL
+```bash
+mkdir -p content && wget -P content https://raw.githubusercontent.com/Gavroshe/RuTweetCorp/master/positive.csv https://raw.githubusercontent.com/Gavroshe/RuTweetCorp/master/negative.csv
